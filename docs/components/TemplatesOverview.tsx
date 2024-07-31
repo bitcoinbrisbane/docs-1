0f57c36e4d4c063e77dfab419a864b5e263321b2
James Lawton <jlaw@horizon.io>
2024-07-31 13:49:47 +0100

    Updated templates with sequence-cli oneliner

--- docs/components/TemplatesOverview.tsx
+++ docs/components/TemplatesOverview.tsx
@@ -1,26 +1,56 @@
 import { SmartLink } from './Landing/SmartLink'
+import { useState } from 'react'
+
+
+
+const CodeSnippet = ({ code }: { code: string }) => (
+  <div>
+  <p className="py-2 font-bold text-themed-secondary text-md font-medium leading-6">
+        Run with Sequence-CLI:
+  </p>
+<div className="vocs_CodeBlock">
+    <div className="vocs_Pre_wrapper">
+      <pre tabIndex={0} className="shiki shiki-themes github-light github-dark-dimmed vocs_Pre" style={{
+        backgroundColor: 'rgb(255, 255, 255)',
+        '--shiki-dark-bg': '#22272e',
+        color: 'rgb(36, 41, 46)',
+        '--shiki-dark': '#adbac7'
+      }}>
+        <code className="vocs_Code">
+          <span className="line vocs_Span">
+            <span className="vocs_Span" style={{
+              color: 'rgb(3, 47, 98)',
+              '--shiki-dark': '#96D0FF'
+            }}>{code}</span>
+          </span>
+        </code>
+      </pre>
+    </div>
+  </div>
+  </div>
+)
 
 export const TemplatesOverview = () => (
+
   <section className="flex flex-col gap-5 border-t dark:border-white-10 border-black-10">
     <div className="flex flex-col gap-1 py-5">
       <h3 className="text-3xl text-themed-primary font-bold">Templates</h3>
       <p className="text-md leading-6 font-medium dark:text-white-80 text-black-80 mr-8">
-        Kickstart your Sequence integration with our open source code demos, examples, and boilerplates to accelerate your
-        time to market.
+        Kickstart your Sequence integration with our open source code demos, examples, and
+        boilerplates to accelerate your time to market.
       </p>
     </div>
     <div className="flex flex-wrap gap-4 md:flex-row flex-col">
       <h4 className="text-2xl text-themed-primary font-bold pt-5">Onboarding</h4>
     </div>
     <div className="flex flex-wrap gap-4 md:flex-row flex-col">
-
       <TemplateCard
         title="Sequence Kit w/ Embedded Wallet for React Boilerplate"
         body="Example boilerplate showcasing embedded wallet integrated with Sequence Kit in a simple React application."
         link="https://github.com/0xsequence/kit-embedded-wallet-react-boilerplate"
         demolink="https://kit-embedded-wallet-react-boilerplate.pages.dev/"
+        oneliner="npx sequence-cli boilerplates create-embedded-wallet-react-starter"
       />
-
       <TemplateCard
         title="Sequence Kit w/ Embedded Wallet for NextJS Boilerplate"
         body="Example boilerplate showcasing embedded wallet integrated with Sequence Kit in a simple NextJS application."
@@ -31,20 +61,20 @@ export const TemplatesOverview = () => (
         title="Embedded Wallet Boilerplate for Wallet Verification on a backend."
         body="Example for your web application to verify the ownership of an Embedded Wallet by passing a JWT and validating against the Sequence API."
         link="https://github.com/0xsequence-demos/embedded-wallet-verify-session"
-      />    
+      />
 
       <TemplateCard
-      title="End to End playground for Sequence Kit"
-      body="Example implementation in React & NextJS showcasing various common patterns using Sequence Kit such as signingv messages, sending transactions, displaying inventory, or minting an NFT."
-      link="https://github.com/0xsequence/kit/tree/master/examples"
-      demolink="https://0xsequence.github.io/kit/"
-    />
-    <TemplateCard
-      title="Embedded Wallet Demo using Wallet Linking"
-      body="Example application to allow players to link other EOA wallets they own for transferring assets or for verification purposes. The demo can be used by clicking EOA Link via the demo button below to see the flow."
-      link="https://github.com/0xsequence-demos/demo-waas-wallet-link"
-      demolink="https://0xsequence.github.io/demo-waas-auth/"
-    />
+        title="End to End playground for Sequence Kit"
+        body="Example implementation in React & NextJS showcasing various common patterns using Sequence Kit such as signingv messages, sending transactions, displaying inventory, or minting an NFT."
+        link="https://github.com/0xsequence/kit/tree/master/examples"
+        demolink="https://0xsequence.github.io/kit/"
+      />
+      <TemplateCard
+        title="Embedded Wallet Demo using Wallet Linking"
+        body="Example application to allow players to link other EOA wallets they own for transferring assets or for verification purposes. The demo can be used by clicking EOA Link via the demo button below to see the flow."
+        link="https://github.com/0xsequence-demos/demo-waas-wallet-link"
+        demolink="https://0xsequence.github.io/demo-waas-auth/"
+      />
     </div>
     <div className="flex flex-wrap gap-4 md:flex-row flex-col">
       <h4 className="text-2xl text-themed-primary font-bold">Monetization</h4>
@@ -77,6 +107,7 @@ const TemplateCard = ({
   deployLink,
   image,
   demolink,
+  oneliner
 }: {
   title: string
   body: string
@@ -84,6 +115,7 @@ const TemplateCard = ({
   deployLink?: string
   image?: string
   demolink?: string
+  oneliner?: string
 }) => {
   return (
     <div className="flex flex-col gap-4 p-4 dark:bg-white-10 bg-white rounded-xl md:w-[calc(50%-8px)]">
@@ -94,6 +126,7 @@ const TemplateCard = ({
       )}
       <p className="text-xl leading-7 text-themed-primary font-bold">{title}</p>
       <p className="text-themed-secondary text-md font-medium leading-6">{body}</p>
+      {oneliner && <CodeSnippet code={oneliner} />}
       <div className="flex gap-4">
         <SmartLink
           href={link}
